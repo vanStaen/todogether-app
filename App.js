a8cb06e2d9c79725f0c7bfdb99fb2950822f31fb
vanStaen <clement.vanstaen@gmail.com>
2023-12-08 14:43:01 +0100

    a div is now called view

--- App.js
+++ App.js
@@ -12,8 +12,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#d9d6cb',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#d9d6cb",
+    alignItems: "center",
+    justifyContent: "center",
+    border: "1px solid blue",
   },
 });
--- components/Title/Title.jsx
+++ components/Title/Title.jsx
@@ -1,15 +1,19 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export const Title = () => {
   return (
-    <>
+    <View style={styles.titleContainer}>
       <Text style={styles.title}>Todogether App </Text>
       <Text style={styles.motto}>Multiplayer-todolists</Text>
-    </>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  titleContainer: {
+    border: "1px solid red",
+    alignItems: "center",
+  },
   title: {
     opacity: 0.5,
     fontWeight: "500",
